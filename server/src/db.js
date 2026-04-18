063fee35e3609222a857b19fc90dc2ad61cecc86
Saad Ardati <saadodi44@gmail.com>
2026-04-18 13:05:46 +0300

    chore: Update db env variable keys

--- server/src/db.js
+++ server/src/db.js
@@ -11,9 +11,9 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') })
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2' } : undefined,
 })
 
